e0bde598c9017834e7c9543f35eafb4dc0e88e29
Alexey Savelyev <savelyev.main@gmail.com>
2023-01-22 21:47:59 +0600

    Base RelayCommand class

---
+++ BankApplication/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace BankApplication.ViewModels.Base
+{
+    internal class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _CanExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object? parameter) => _Execute(parameter);
+    }
+}
