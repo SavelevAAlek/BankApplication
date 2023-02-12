6306f883d1a225cf040f09af9eae060a8494d31e
Alexey Savelyev <savelyev.main@gmail.com>
2023-02-12 22:57:59 +0600

    models

---
+++ BankApplication/Models/Client.cs
@@ -0,0 +1,29 @@
+﻿using BankApplication.ViewModels.Base;
+
+namespace BankApplication.Models
+{
+    public class Client : ViewModelBase
+    {
+        private string? _name;
+        private string? _surname;
+        private string? _patronymic;
+        private string? _phoneNumber;
+        private string? _passportData;
+
+        public string? Name { get => _name; set => SetProperty(ref _name, value); }
+        public string? Surname { get => _surname; set => SetProperty(ref _surname, value); }
+        public string? Patronymic { get => _patronymic; set => SetProperty(ref _patronymic, value); }
+        public string? PhoneNumber { get => _phoneNumber; set => SetProperty(ref _phoneNumber, value); }
+        public string? PassportData { get => _passportData; set => SetProperty(ref _passportData, value); }
+
+        public Client() { }
+        public Client(string? name, string? surname, string? patronymic, string? phoneNumber, string? passportData)
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            PhoneNumber = phoneNumber;
+            PassportData = passportData;
+        }   
+    }
+}
---
+++ BankApplication/Models/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication.Models
+{
+    public class Manager : Employee, IEmployee
+    {
+        public void ChangePhoneNumber(Client client, string phoneNumber) => client.PhoneNumber = phoneNumber;
+
+        public void DetermineAccessLevel(Employee employee)
+        {
+        }
+    }
+}
